fb99ff6f3b1c4c873f66e97598ef89fed7ef2564
Pritam96 <pritammondal96@gmail.com>
2023-08-21 21:37:00 +0530

    Initial Express setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
