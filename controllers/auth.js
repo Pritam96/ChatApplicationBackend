db19ee3063053df6645d6b8b3c5cb95c54c82861
Pritam96 <pritammondal96@gmail.com>
2023-08-22 22:34:00 +0530

    AsyncAwait middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,14 @@
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 // @desc    Register user
 // @route   GET /api/v1/auth/register
 // @access  Public
-exports.register = async (req, res, next) => {
-  try {
-    const { name, email, phone, password } = req.body;
+exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, phone, password } = req.body;
 
-    // Create user
-    const user = await User.create({ name, email, phone, password });
-    res.status(200).json({ success: true });
-  } catch (error) {
-    next(error);
-  }
-};
+  // Create user
+  const user = await User.create({ name, email, phone, password });
+  res.status(200).json({ success: true });
+});
